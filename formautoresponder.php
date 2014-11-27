25fa4b97fe28217cd91af6464e6a07bcd9cfebc1
cmrandall <cmrandall@yahoo.com>
2014-11-27 15:17:10 -0500

    adding skinnywithmel.info code to GitHub

---
+++ formautoresponder.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml/dtd/xhtml11-strict.dtd">
+<html xmlns="http://www/w3.org/1999/xhtml">
+	<head>
+		<style type="text/css">
+			h1 {font-size:18px;font-weight:normal;color:#224568;}
+			p {font-size:14px;}
+			.display_label {color:#555555;font-weight:bold;width:90px;vertical-align:top;}
+			.display_data {color:#555555;font-weight:normal;}
+			table td {border:1px solid #999;width:auto;padding:5px}
+			table td.noborderbottom {border-bottom:none;padding-bottom:0px;}
+			table td.nobordertop {border-top:none;}
+			.msgcontent {padding-left:20px;}
+			table * {font-size:14px;}
+		</style>
+	</head>
+
+<body>
+	<div id="wrapper">
+		<div class="pagecontent">
+			<div class="msgcontent">
+    		<h1>Thank You for Your Inquiry</h1>
+			<div class="leftcontent">
+      			<p>Thank you for your interest in cmrandall.com.  Your request has been received and you should be contacted within 48 hours.</p>
+				<p>Below is a copy of the information you sent to us.</p>
+			</div>
+			<div style="clear:both"></div>
+			<table cellpadding="0" cellspacing="0" border="0" style="border-collapse:collapse;">
+				<tr>
+					<td class="display_label">
+						Name:
+					</td>
+					<td class="display_data">
+						ff<name>
+					</td>
+				</tr>
+				<tr>
+					<td class="display_label">
+						Email:
+					</td>
+					<td class="display_data">
+						ff<email>
+					</td>
+				</tr>
+				<tr>
+					<td class="display_label">
+						Message:
+					</td>
+					<td class="display_data">
+						ff<message>
+					</td>
+				</tr>
+			</table>
+			<p><a href="http://www.cmrandall.com">cmrandall.com</a></p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
